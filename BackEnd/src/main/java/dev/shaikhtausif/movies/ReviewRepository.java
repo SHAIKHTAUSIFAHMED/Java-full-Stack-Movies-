48411235c15ad70f6cea370e3f5f073e851f70db
SHAIKHTAUSIFAHMED <114562269+SHAIKHTAUSIFAHMED@users.noreply.github.com>
2024-07-10 22:05:19 +0530

    Delete BackEnd directory

--- BackEnd/src/main/java/dev/shaikhtausif/movies/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.shaikhtausif.movies;
-
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
-}
